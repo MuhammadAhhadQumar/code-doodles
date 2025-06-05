c3afc9e6bbc362329f6c31b0ecac7a4b91ec2dbf
Ahhad <ahhadqamar1@gmail.com>
2025-06-05 14:57:06 +0000

    Initialize React application with Vite and Tailwind CSS

---
+++ apple/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="App bg-red-500 text-white text-2xl text-center font-bold p-5 ">
+      <h1>Welcome to the Apple App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+export default App;
